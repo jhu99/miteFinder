e0dd5afca1bd9c21ea3ec5b7348424feeaf316dd
Jialu Hu <jialu@Ox.local>
2017-05-25 22:05:35 +0800

    change the framework of the project

--- tests/miteFinderTest.cpp
+++ tests/miteFinderTest.cpp
@@ -11,30 +11,35 @@
 #include "genome.h"
 #include "mite.h"
 #include "mite_finder.h"
+#include "pattern_learning.h"
 #include "argparser.h"
 
 int main(int argc, const char * argv[]) {
     std::string filename="/Users/jialu/Research/datasets/OSgenomeV6.man";
-    bool enable_mismatch=true;
-	double seconds=0;
-    Genome osgenome;
-    ArgParser myparser(argc,argv);
-	time_t start_time,end_time;
-	time(&start_time);
-	osgenome.readSequence(filename);
+    //bool enable_mismatch=true;
+	//double seconds=0;
+    //Genome osgenome;
+    //ArgParser myparser(argc,argv);
+	//time_t start_time,end_time;
+	//time(&start_time);
+	std::fstream output("/Users/jialu/Research/datasets/kmer_count.txt",std::ios::out);
+	patternLearning("/Users/jialu/Research/datasets/matchresult.fsa", "/Users/jialu/Research/datasets/mismatchresult.fsa",output);
+	//test(filename);
+	/*osgenome.readSequence(filename);
     int numChr=osgenome.getNumChrom();
     Seed_set tset;
     std::fstream output;
     output.open("/Users/jialu/Research/datasets/mite_candidates.fsa",std::ios_base::out);
     for(int i=0;i<numChr;i++) {
         char* pchr=osgenome.getChrom(i);
-        mite_finder(tset,pchr,enable_mismatch,10000,MIN_LENGTH_TIR);
+		mite_finder(tset,pchr,enable_mismatch,10000,MIN_LENGTH_TIR);
         write_seed(tset,pchr,output,i+1);
         tset.clear();
     }
 	time(&end_time);
 	seconds= difftime(end_time, start_time);
 	std::cout << "The program cost "<<seconds<<" seconds totally to search for MITEs."<<std::endl;
-    output.close();
+    output.close();*/
+	output.close();
     return 0;
 }
---
+++ tests/patternTest.cpp
@@ -0,0 +1,17 @@
+//
+//  patternTest.cpp
+//  miteFinder
+//
+//  Created by Jialu Hu on 2017/5/25.
+//  Copyright © 2017年 Jialu Hu. All rights reserved.
+//
+
+#include <stdio.h>
+#include "patter_learning.h"
+
+int main(int argc, const char * argv[]) {
+	std::string filename="/Users/jialu/Research/datasets/OSgenomeV6.man";
+	std::fstream output("/Users/jialu/Research/datasets/kmer_count.txt",std::ios::out);
+	patternLearning("/Users/jialu/Research/datasets/matchresult.fsa", "/Users/jialu/Research/datasets/mismatchresult.fsa",output);
+	return 0;
+}
