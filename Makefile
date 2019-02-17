b04da53b1cabf82cb816aed863da6f95ac3409f8
Jialu Hu <jhu@nwpu.edu.cn>
2019-02-17 12:43:27 -0500

    Manual update

--- Makefile
+++ Makefile
@@ -17,12 +17,15 @@ CXXFLAGS = -Wall -g3 -DDEBUG -std=c++0x -DVERBOSE -Ilib/
 else
 CXXFLAGS = -Wall -O3 -ffast-math -Ilib/ -std=c++0x -DNDEBUG
 endif
-all: miteFinderTest patternLearningTest ArgParserTest
-miteFinderTest: tests/miteFinderTest.cpp lib/genome.o lib/mite.o lib/argparser.o
+all: miteFinder
+#patternLearningTest ArgParserTest move
+miteFinder: tests/miteFinderTest.cpp lib/genome.o lib/mite.o lib/argparser.o
 	${CXX} ${CXXFLAGS} -o $@ $^
 patternLearningTest: tests/patternTest.cpp lib/genome.o
 	${CXX} ${CXXFLAGS} -o $@ $^
 ArgParserTest: tests/testargparser.cpp lib/argparser.o
 	${CXX} ${CXXFLAGS} -o $@ $^
+move:
+	mv miteFinder ./bin/
 clean:
 	rm lib/*.o
--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-WORKFOLDER=/Users/jialu/Research/miteFinder
-DATAFOLDER=/Users/jialu/Research/datasets/
-#$WORKFOLDER/miteFinderTest -input $DATAFOLDER/OSgenomeV6.man -pattern_scoring $WORKFOLDER/profile/pattern_scoring.txt -threshold 0.5 -output $DATAFOLDER/mite/OS_mite_candidates.fsa
-$WORKFOLDER/miteFinderTest -input $DATAFOLDER/ftp.ncbi.nlm.nih.gov/genomes/all/GCF/GCF_000001405.37_GRCh38.p11_genomic.fna -pattern_scoring $WORKFOLDER/profile/pattern_scoring.txt -threshold 0.5 -output $DATAFOLDER/mite/Human_mite_candidates.fsa
+input_filename = $1
+output_filename = $2
+./miteFinder -input $1 -output $2 -pattern_scoring ./profile/pattern_scoring.txt -threshold 0.5
